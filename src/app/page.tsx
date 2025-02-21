122d227cadb85f744f2099b3931cf9d7e118a101
sohey-dr <heyso1209@gmail.com>
2025-02-22 03:31:57 +0900

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { CameraComponent } from '@/components/Camera';
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen">
+    <main className="h-[100dvh] w-screen">
       <CameraComponent />
     </main>
   );
