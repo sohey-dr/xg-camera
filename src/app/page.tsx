2e9a3a7982b6a1de4dd55d15a908de9f500d43f3
sohey-dr <heyso1209@gmail.com>
2025-02-22 03:26:05 +0900

    x

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { CameraComponent } from '@/components/Camera';
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen overflow-hidden">
+    <main className="h-screen w-screen">
       <CameraComponent />
     </main>
   );
